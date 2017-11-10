3542502c25b8d5a73c84fc74d40ac033a40fc720
RobinSJennings <31321511+RobinSJennings@users.noreply.github.com>
2017-11-09 21:06:03 -0600

    Add files via upload

---
+++ selectOneEvent.php
@@ -0,0 +1,47 @@
+<?php
+
+echo "<table style='border: solid thick red;'>";
+ echo "<tr><th><h1>Name of contact</h1></th></tr>";
+
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: thick solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+		session_start();
+
+	$servername = "localhost";
+	$username = "nephilim42_wdv";
+	$password = "nephilim42";
+	$dbname = "nephilim42_wdv";
+	
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT contact_name FROM wdv_341_customer_contacts"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
